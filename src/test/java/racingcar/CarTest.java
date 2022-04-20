cff358a2a71352a7dc5262efa8d98c15f3856467
viiviii <viiiix.viiviii@gmail.com>
2022-04-20 17:40:30 +0900

    test: CarTest 리팩토링

--- src/test/java/racingcar/CarTest.java
+++ src/test/java/racingcar/CarTest.java
@@ -1,46 +1,67 @@
 package racingcar;
 
 import camp.nextstep.edu.missionutils.Randoms;
-import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
 import org.mockito.MockedStatic;
+import org.mockito.stubbing.OngoingStubbing;
+
+import java.util.function.Function;
 
 import static org.assertj.core.api.Assertions.*;
-import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.*;
 
 public class CarTest {
-    
+
+    private Car car;
+
+    @BeforeEach
+    void setUp() {
+        car = new Car();
+    }
+
     @DisplayName("자동차는 전진할 수 있다")
     @Test
-    void move() {
-        try(final MockedStatic<Randoms> mocked = mockStatic(Randoms.class)) {
-            //given
-            Car car = new Car();
-            mocked.when(() -> Randoms.pickNumberInRange(0, 9)).thenReturn(4);
+    void move() throws Throwable {
+        mockingRandomsPickNumberInRange(
+                whenUsing -> whenUsing.thenReturn(4),
+                () -> {
+                    // given
+                    MoveCondition condition = car.run();
 
-            //when
-            MoveCondition condition = car.run();
+                    //when
+                    boolean actual = condition.isMove();
 
-            //then
-            assertThat(condition.isMove()).isTrue();
-        }
+                    //then
+                    assertThat(actual).isTrue();
+                });
     }
 
     @DisplayName("자동차는 멈출 수 있다")
     @Test
-    void stop() {
-        try(final MockedStatic<Randoms> mocked = mockStatic(Randoms.class)) {
-            //given
-            Car car = new Car();
-            mocked.when(() -> Randoms.pickNumberInRange(0, 9)).thenReturn(3);
+    void stop() throws Throwable {
+        mockingRandomsPickNumberInRange(
+                whenUsing -> whenUsing.thenReturn(3),
+                () -> {
+                    // given
+                    MoveCondition condition = car.run();
 
-            //when
-            MoveCondition condition = car.run();
+                    //when
+                    boolean actual = condition.isStop();
 
-            //then
-            assertThat(condition.isStop()).isTrue();
+                    //then
+                    assertThat(actual).isTrue();
+                });
+    }
+
+    private void mockingRandomsPickNumberInRange(
+            Function<OngoingStubbing<Integer>, OngoingStubbing<Integer>> thenReturn,
+            Executable executable) throws Throwable {
+        try (final MockedStatic<Randoms> mocked = mockStatic(Randoms.class)) {
+            thenReturn.apply(mocked.when(() -> Randoms.pickNumberInRange(0, 9)));
+            executable.execute();
         }
     }
 }
