1e0a2dbe85553fe16f2a20cfa28ff3f5c9d875b0
viiviii <viiiix.viiviii@gmail.com>
2022-04-21 09:25:00 +0900

    feat: 자동차 1회 이동상황 출력 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,12 @@
 package racingcar;
 
+import racingcar.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        Car car = new Car();
+        MoveCondition run = car.run();
+        OutputView outputView = new OutputView();
+        outputView.printRacingPace(run);
     }
 }
