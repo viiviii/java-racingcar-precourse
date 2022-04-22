20a35d98bcae5c585e7f437b243eca2ec6565a0c
viiviii <viiiix.viiviii@gmail.com>
2022-04-22 15:25:53 +0900

    test: Position이 음수일 때 예외 처리 테스트

--- src/test/java/racingcar/model/PositionTest.java
+++ src/test/java/racingcar/model/PositionTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.catchThrowable;
 
 public class PositionTest {
 
@@ -18,5 +19,19 @@ public class PositionTest {
 
         //then
         assertThat(position.get()).isOne();
+    }
+
+    @DisplayName("포지션이 음수인 경우 예외가 발생한다")
+    @Test
+    void throwExceptionWhenNegativePosition() {
+        //given
+        int negative = -1;
+
+        //when
+        Throwable thrown = catchThrowable(() -> Position.from(negative));
+
+
+        //then
+        assertThat(thrown).isInstanceOf(IllegalArgumentException.class);
     }
 }
