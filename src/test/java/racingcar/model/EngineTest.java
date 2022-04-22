38b908aca9cdbf5b4a58a25464cc1e1d08617cf4
viiviii <viiiix.viiviii@gmail.com>
2022-04-22 22:16:37 +0900

    test: Engine의 energy 원시 값을 객체로 받게 테스트 작성

--- src/test/java/racingcar/model/EngineTest.java
+++ src/test/java/racingcar/model/EngineTest.java
@@ -8,11 +8,11 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class EngineTest {
     private Engine engine = new Engine();
 
-    @DisplayName("4 이상이면 전진이다")
+    @DisplayName("에너지가 4 이상이면 전진한다")
     @Test
     void forwardWhenMoreThan4() {
         //given
-        int energy = 4; // TODO: 하드코딩 제거
+        Energy energy = new Energy(4); // TODO: 하드코딩 제거
 
         //when
         Move move = engine.powerBy(energy);
@@ -21,11 +21,11 @@ public class EngineTest {
         assertThat(move.isForward()).isTrue();
     }
 
-    @DisplayName("4 미만이면 멈춤이다")
+    @DisplayName("에너지가 4 미만이면 멈춤이다")
     @Test
     void stopWhenLessThan4() {
         //given
-        int energy = 4 - 1; // TODO: 하드코딩 제거
+        Energy energy = new Energy(4 - 1); // TODO: 하드코딩 제거,
 
         //when
         Move move = engine.powerBy(energy);
