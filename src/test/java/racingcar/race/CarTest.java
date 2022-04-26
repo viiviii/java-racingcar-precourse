1f1f985616eefd1c76d20df1cbb272a71869244a
viiviii <viiiix.viiviii@gmail.com>
2022-04-26 14:30:31 +0900

    test: 빠져있던 compareToPosition 테스트 추가

--- src/test/java/racingcar/race/CarTest.java
+++ src/test/java/racingcar/race/CarTest.java
@@ -11,6 +11,49 @@ import static org.assertj.core.api.Assertions.assertThat;
 class CarTest {
     private String name = "apple";
 
+    @DisplayName("다른 자동차와 같은 위치면 0을 리턴한다")
+    @Test
+    void compareToPositionWhenEquals() {
+        //given
+        int samePosition = 3;
+        Car car = Car.of("pobi", samePosition);
+        Car other = Car.of("other", samePosition);
+
+        //when
+        int actual = car.compareToPosition(other);
+
+        //then
+        assertThat(actual).isZero();
+    }
+
+    @DisplayName("다른 자동차보다 위치가 앞에 있으면 1을 리턴한다")
+    @Test
+    void compareToPositionWhenFront() {
+        //given
+        Car car = Car.of("pobi", 5);
+        Car other = Car.of("other", 1);
+
+        //when
+        int actual = car.compareToPosition(other);
+
+        //then
+        assertThat(actual).isEqualTo(1);
+    }
+
+    @DisplayName("다른 자동차보다 위치가 뒤에 있으면 -1을 리턴한다")
+    @Test
+    void compareToPositionWhenBack() {
+        //given
+        Car car = Car.of("pobi", 1);
+        Car other = Car.of("other", 5);
+
+        //when
+        int actual = car.compareToPosition(other);
+
+        //then
+        assertThat(actual).isEqualTo(-1);
+    }
+
     @DisplayName("자동차가 해당 위치에 있으면 true")
     @Test
     void returnTrueWhenSamePosition() {
@@ -25,7 +68,7 @@ class CarTest {
         assertThat(actual).isTrue();
     }
 
-    @DisplayName("자동차의 위치가 다르면 false 리턴")
+    @DisplayName("자동차가 해당 위치가 아니면 false를 리턴한다")
     @Test
     void returnFalseWhenDifferencePosition() {
         //given
