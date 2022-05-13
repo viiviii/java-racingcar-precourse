a8d1693e75123c060f62100f53bfe7a7f3a1c79d
viiviii <viiiix.viiviii@gmail.com>
2022-05-13 14:41:35 +0900

    refactor: 우승 조건인 최대 거리를 WinnerReferee가 담당

--- src/main/java/racingcar/race/WinnersReferee.java
+++ src/main/java/racingcar/race/WinnersReferee.java
@@ -4,20 +4,26 @@ import racingcar.rule.Name;
 import racingcar.rule.Position;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.function.Predicate;
 
 public final class WinnersReferee {
 
     // TODO
     public Winners determineFrom(MoveRecords moveRecords) {
-        final Position winningPosition = moveRecords.maxPosition();
-        final List<Name> winnerNames = new ArrayList<>();
+        final Predicate<Name> isWinner = criteriaBy(moveRecords);
+        final List<Name> winners = new ArrayList<>();
         for (Name name : moveRecords.allNames()) {
-            final Position position = moveRecords.positionBy(name);
-            if (position.equals(winningPosition)) {
-                winnerNames.add(name);
+            if (isWinner.test(name)) {
+                winners.add(name);
             }
         }
-        return Winners.from(winnerNames);
+        return Winners.from(winners);
+    }
+
+    private Predicate<Name> criteriaBy(MoveRecords moveRecords) {
+        final Position winningPosition = Collections.max(moveRecords.allPositions());
+        return name -> moveRecords.positionBy(name).equals(winningPosition);
     }
 }
--- src/test/java/racingcar/race/WinnersRefereeTest.java
+++ src/test/java/racingcar/race/WinnersRefereeTest.java
@@ -9,18 +9,18 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class WinnersRefereeTest {
 
-    @DisplayName("가장 먼 위치의 자동차가 우승이다")
+    @DisplayName("우승자를 판별한다")
     @Test
     void winners() {
         //given
         Name expectedWinner = new Name("crong");
         MoveRecords moveRecords = new MoveRecords();
-        moveRecords.recordOf(new Name("pobi"), new Position(1));
+        moveRecords.recordOf(new Name("name1"), new Position(1));
         moveRecords.recordOf(expectedWinner, new Position(9));
-        moveRecords.recordOf(new Name("honux"), new Position(3));
-        WinnersReferee winnersReferee = new WinnersReferee();
+        moveRecords.recordOf(new Name("name2"), new Position(3));
 
         //when
+        WinnersReferee winnersReferee = new WinnersReferee();
         Winners winners = winnersReferee.determineFrom(moveRecords);
 
         //then
