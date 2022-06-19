d7c897373a6ee60e82b0b557e3c74dd833f3474a
viiviii <viiiix.viiviii@gmail.com>
2022-06-19 23:33:17 +0900

    :white_check_mark: 이동 결과를 view에 출력한다

--- src/test/java/racingcar/ControllerTest.java
+++ src/test/java/racingcar/ControllerTest.java
@@ -2,41 +2,51 @@ package racingcar;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.mockito.BDDMockito;
 
+import static org.mockito.ArgumentMatchers.anyBoolean;
+import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 
 class ControllerTest {
     private final Car car = mock(Car.class);
-    private final Controller controller = new Controller(car);
+    private final View view = mock(View.class);
+    private final Controller controller = new Controller(view, car);
 
     @DisplayName("1대의 자동차가 1번 움직인다")
     @Test
     void start() {
         //given
-        BDDMockito.given(car.move()).willReturn(true);
+        given(car.move()).willReturn(true);
 
         //when
         controller.start();
 
         //then
         verify(car).move();
+        verify(view).moveResult(anyBoolean());
     }
 
     public static final class Controller {
+        private final View view;
         private final Car car;
 
-        public Controller(Car car) {
+        public Controller(View view, Car car) {
+            this.view = view;
             this.car = car;
         }
 
         public void start() {
             boolean moved = car.move();
+            view.moveResult(moved);
         }
     }
 
     public interface Car {
         boolean move();
+    }
+
+    public interface View {
+        void moveResult(boolean moved);
     }
 }
