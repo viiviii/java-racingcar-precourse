04dc312fd9ea5386dda1c4abc3533fb2c984e9d9
viiviii <viiiix.viiviii@gmail.com>
2022-06-21 15:13:48 +0900

    :recycle: 테스트 보강

--- src/test/java/racingcar/RaceTest.java
+++ src/test/java/racingcar/RaceTest.java
@@ -12,14 +12,16 @@ import static org.mockito.Mockito.mock;
 
 class RaceTest {
     private final MyRandom myRandom = mock(MyRandom.class);
+    private final int STOP = 3;
+    private final int FORWARD = 4;
 
     @DisplayName("자동차가 여러번 움직인다")
     @Test
-    void moves() throws Exception {
+    void moves() {
+
         //given
-        int moveTimes = 2;
-        int forward = 4;
-        given(myRandom.pickNumberInRage(anyInt(), anyInt())).willReturn(forward);
+        int moveTimes = 5;
+        given(myRandom.pickNumberInRage(anyInt(), anyInt())).willReturn(FORWARD, STOP, FORWARD);
 
         //when
         Car car = new CarImpl();
@@ -27,7 +29,8 @@ class RaceTest {
         List<Integer> positions = race.movesBy(moveTimes);
 
         //then
-        assertThat(positions).hasSize(moveTimes);
-        assertThat(positions).containsExactly(1, 2);
+        assertThat(positions)
+                .hasSize(moveTimes)
+                .containsExactly(1, 1, 2, 3, 4);
     }
 }
