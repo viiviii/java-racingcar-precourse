e22aef9d0523c6a2f0fc6296b161a8ab664ebfa0
viiviii <viiiix.viiviii@gmail.com>
2022-06-23 12:47:16 +0900

    :recycle: 패키지 의존성을 한 방향으로 바꾸기 (#34)
    * :sparkles: factory를 주입받아 controller에서 사용하게 구현
    
    * :construction: 랜덤 값을 Cars -> Car로 옮기기
    
    * :rewind: ":construction: 랜덤 값을 Cars -> Car로 옮기기"
    
    This reverts commit 15ea3f2ee530f38fefe136c6c4e79d30f79ee7ee.
    
    * :truck: 시도횟수 구체 클래스 gameStragegy -> gamePlay로 이동
    
    * :recycle: 시도횟수의 인터페이스는 제거하고 구체 클래스로 대체
    
    * :recycle: 시도횟수 테스트 리팩토링
    
    * :fire: 완료한 TODO 주석 제거

--- src/test/java/racingcar/AttemptCountTest.java
+++ src/test/java/racingcar/AttemptCountTest.java
@@ -3,41 +3,60 @@ package racingcar;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import racingcar.gamePlay.AttemptCount;
-import racingcar.gamePlay.Car;
 import racingcar.gamePlay.Cars;
 import racingcar.gamePlay.RaceResult;
-import racingcar.gameStrategy.AttemptCountImpl;
-import racingcar.gameStrategy.CarImpl;
-import racingcar.gameStrategy.RandomMoveCars;
+import racingcar.gamePlay.Record;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.BDDMockito.given;
-import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.*;
 
 class AttemptCountTest {
 
-    private final MyRandom myRandom = mock(MyRandom.class); // TODO
-    private final int FORWARD = 4;
-
+    private final Cars cars = mock(Cars.class);
 
     @DisplayName("이동 횟수 만큼 자동차들이 이동한다")
     @Test
     void moveCars() {
         //given
         int count = 5;
-        given(myRandom.pickNumberInRage(anyInt(), anyInt())).willReturn(FORWARD); // TODO
 
         //when
-        AttemptCount attemptCount = new AttemptCountImpl(count);
-        Cars cars = RandomMoveCars.of(myRandom, createCar("pobi"));
-        RaceResult raceResult = attemptCount.move(cars);
+        new AttemptCount(count).move(cars);
 
         //then
-        assertThat(raceResult.attemptCount()).isEqualTo(count);
+        verify(cars, times(count)).move();
     }
 
-    private Car createCar(String carName) {
-        return new CarImpl(carName);
+    @DisplayName("자동차들의 이동 기록 목록을 리턴한다")
+    @Test
+    void result() {
+        //given
+        int count = 1;
+        StubRecord record = new StubRecord();
+        given(cars.move()).willReturn(record);
+
+        //when
+        RaceResult result = new AttemptCount(count).move(cars);
+
+        //then
+        assertThat(result.attemptCount()).isEqualTo(count);
+        assertThat(result.recordOf(0)).isEqualTo(record); // TODO: index 0, count 1이 이상함
+    }
+
+    private static final class StubRecord implements Record {
+        @Override
+        public Set<String> carNames() {
+            return new HashSet<>(Arrays.asList("pobi"));
+        }
+
+        @Override
+        public int positionBy(String carName) {
+            return 1;
+        }
     }
 }
